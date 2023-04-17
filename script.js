1510a71404857435445ad0dcf2c79bfde47b1ec7
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-04-17 22:07:33 +0100

    character types question added, along with master concat array and loop to generate characters

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
 
 function generatePassword() {
 // Arrays for for password criteria
@@ -22,18 +24,55 @@ function generatePassword() {
         alert("Password must be between 8-128 characters, please try again")
         return;
     }
+// window.confirm method used to display dialog message on whether or not to include lowercase, uppercase, numeric, and/or special characters
+    passwordUpper = confirm("Click OK to if you would like to include lowercase characters");
+    passwordLower = confirm("Click OK to if you would like to include uppercase characters");
+    passwordNumber = confirm("Click OK to if you would like to include numeric characters");    
+    passwordSpecial  = confirm("Click OK to if you would like to include special characters");
 
+ // If user selects cancel on all options, This alert will come up telling user they must select atleast one option   
+    if (passwordUpper === false && passwordLower === false && passwordNumber === false && passwordSpecial === false) {
+        alert("You must choose at least one option");
+    return;
+    }
+ 
+    // delcaring variable for an array combining numbers, uppercase, lowercase & special characters 
+    var passwordCharacters = []
+      
+    if (passwordUpper) {
+      passwordCharacters = passwordCharacters.concat(upper)
+    }
+
+    if (passwordLower) {
+      passwordCharacters = passwordCharacters.concat(lower)
+    }
+      
+    if (passwordNumber) {
+      passwordCharacters = passwordCharacters.concat(numbers)
+    }
+
+    if (passwordSpecial) {
+      passwordCharacters = passwordCharacters.concat(special)
+    }
+
+     // This string variable will be filled based on the for loop selecting random characters from the array
+     //Math.random method used to generate random number. Math.floor to round to decimal
+     var password = "";
+      
+     for (var i = 0; i < passwordLength; i++) {
+       password = password + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+       console.log(password)
+     }
+     return password;
+}
 
 // Write password to the #password input
-function writePassword() {
- var password = generatePassword();
- var passwordText = document.querySelector("#password");
+     function writePassword() {
+     var password = generatePassword();
+     var passwordText = document.querySelector("#password");
 
- passwordText.value = password;
-}
+    passwordText.value = password;
+    }
 
-  
-  // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-}
+
